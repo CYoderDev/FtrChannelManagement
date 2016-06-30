948ae8483d222bd913637e92ed96c34d8d73081f
cyy132 <cyy132@ftr.com>
2016-06-30 17:38:58 -0400

    Added LdapAuthentication

---
+++ FrontierVOps.Data/FrontierVOps.Security/FormsAuth/LdapAuthentication.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.Linq;
+using System.Security;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrontierVOps.Security.FormsAuth
+{
+    public class LdapAuthentication
+    {
+        private string _path;
+        private string _filterAttribute;
+
+        public LdapAuthentication(string path)
+        {
+            this._path = path;
+        }
+
+        public bool IsAuthenticated(string domain, string username, SecureString EncryptedPass, string Key)
+        {
+            string domainUser = domain + @"\" + username;
+            DirectoryEntry entry = new DirectoryEntry(this._path, domainUser, Crypto.Decrypt(EncryptedPass, Key, Algorithm.Rijndael));
+
+            try
+            {
+                object obj = entry.NativeObject;
+
+                DirectorySearcher search = new DirectorySearcher(entry);
+
+                search.Filter = "(SAMAccountName=" + username + ")";
+                search.PropertiesToLoad.Add("cn");
+                SearchResult result = search.FindOne();
+
+                if (null == result)
+                    return false;
+
+                this._path = result.Path;
+                this._filterAttribute = (string)result.Properties["cn"][0];
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error authenticating user. " + ex.Message);
+            }
+            return true;
+        }
+
+        public string GetGroups()
+        {
+            DirectorySearcher search = new DirectorySearcher(this._path);
+            search.Filter = "(cn=" + this._filterAttribute + ")";
+            search.PropertiesToLoad.Add("memberOf");
+            StringBuilder groupNames = new StringBuilder();
+
+            try
+            {
+                SearchResult result = search.FindOne();
+                int propertyCount = result.Properties["memberOf"].Count;
+                string dn;
+                int equalsIndex, commaIndex;
+
+                for (int propertyCounter = 0; propertyCounter < propertyCount; propertyCounter++)
+                {
+                    dn = (string)result.Properties["memberOf"][propertyCounter];
+                    equalsIndex = dn.IndexOf("=", 1);
+                    commaIndex = dn.IndexOf(",", 1);
+                    
+                    if (-1 == equalsIndex)
+                        return null;
+
+                    groupNames.Append(dn.Substring((equalsIndex + 1), (commaIndex - equalsIndex) - 1));
+                    groupNames.Append("|");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error obtaining group names. " + ex.Message);
+            }
+            return groupNames.ToString();
+        }
+    }
+}
