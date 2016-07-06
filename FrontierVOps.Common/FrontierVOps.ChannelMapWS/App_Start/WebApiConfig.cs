145c2404c3ab1184612b3f8f0a3a67d623297e82
cyy132 <cameron.yoder@ftr.com>
2016-07-06 14:39:48 -0400

    App Settings  and Security Web.Config update

--- FrontierVOps.Common/FrontierVOps.ChannelMapWS/App_Start/WebApiConfig.cs
+++ FrontierVOps.Common/FrontierVOps.ChannelMapWS/App_Start/WebApiConfig.cs
@@ -24,6 +24,7 @@ namespace FrontierVOps.ChannelMapWS
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.Filters.Add(new AuthorizeAttribute());
 
             // Web API routes
             config.MapHttpAttributeRoutes();
