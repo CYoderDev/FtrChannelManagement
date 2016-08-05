c134eba2360b2819dc855b1afe163b8022a0211b
cyy132 <cameron.yoder@ftr.com>
2016-08-05 14:15:18 -0400

    Add config project

---
+++ FrontierVOps.Config/FrontierVOps.Config/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrontierVOps.Config
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
