8a0894eea1769037f2f6e0180caf6328228ad8c5
cyy132 <cameron.yoder@ftr.com>
2016-08-31 10:21:12 -0400

    Added Server Layout

---
+++ FrontierVOps.FiOS.Servers/FrontierVOps.FiOS.Servers/Controllers/ServerMgr.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FrontierVOps.FiOS.Servers.Enumerators;
+using FrontierVOps.FiOS.Servers.Objects;
+
+namespace FrontierVOps.FiOS.Servers.Controllers
+{
+    public class ServerMgr
+    {
+        
+    }
+}
---
+++ FrontierVOps.FiOS.Servers/FrontierVOps.FiOS.Servers/Enumerators/FiOSServers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FrontierVOps.FiOS.Servers.Objects;
+
+namespace FrontierVOps.FiOS.Servers.Enumerators
+{
+    public class FiOSServers : IEnumerable<FiOSServer>
+    {
+        List<FiOSServer> servers;
+
+        public IEnumerator<FiOSServer> GetEnumerator()
+        {
+            return servers.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
