260dc75b183aecdbae5d7851664912d2407d07d1
cyy132 <cameron.yoder@ftr.com>
2016-09-06 11:19:21 -0400

    Populated objects

---
+++ FrontierVOps.FiOS.HealthCheckConsole/FrontierVOps.FiOS.HealthCheck.DataObjects/HTMLFormatter.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrontierVOps.FiOS.HealthCheck.DataObjects
+{
+    public class HTMLFormatter
+    {
+        private StringBuilder _strBuilder { get; set; }
+        private bool _isTableStarted { get; set; }
+        private bool _isLastRowError { get; set; }
+
+        public HTMLFormatter()
+        {
+            _strBuilder = new StringBuilder();
+            _isTableStarted = false;
+            _isLastRowError = false;
+        }
+
+        public void SetRole(string Role)
+        {
+            _strBuilder.AppendLine("<p style=\"font-size: 150%; font-weight: bold; font-variant: small-caps; padding: 4px;\">");
+            _strBuilder.AppendFormat("<u>{0}</u>", Role.ToLower());
+            _strBuilder.AppendLine("</p>");
+        }
+
+        public void BeginTable(string ServerName)
+        {
+            _strBuilder.AppendLine("<div><br /></div>");
+            _strBuilder.AppendLine("<table border=\"0\" style=\"border-collapse: collapse; width: 65%;\">");
+            _strBuilder.AppendLine("<th colspan=\"3\" style=\"font-size: 110%; padding: 4px; font-variant: small-caps; background: black; color: white;\">");
+            _strBuilder.Append(ServerName.ToLower());
+            _strBuilder.AppendLine("</th>");
+            this._isTableStarted = true;
+        }
+
+        public void AddStatusRow(string CheckName, StatusResult Result)
+        {
+            checkIsTableStarted();
+
+            _strBuilder.AppendLine("<tr style=\"border: 1px solid black\">");
+            _strBuilder.AppendLine("<td style=\"padding: 1px; border: 1px solid black\">");
+            _strBuilder.Append(CheckName);
+            _strBuilder.AppendLine("</td>");
+            _strBuilder.Append("<td style=\"border: 1px solid black; padding: 2px; background:");
+
+            switch (Result)
+            {
+                case StatusResult.Ok:
+                    _strBuilder.Append("#4bc659;\">");
+                    this._isLastRowError = false;
+                    break;
+                case StatusResult.Warning:
+                    _strBuilder.Append("#efad43;\">");
+                    this._isLastRowError = false;
+                    break;
+                case StatusResult.Error:
+                    _strBuilder.Append("#c64b4b;\">");
+                    this._isLastRowError = true;
+                    break;
+                case StatusResult.Critical:
+                    _strBuilder.Append("#e50b0b; font-weight: bold;\">");
+                    this._isLastRowError = true;
+                    break;
+            }
+
+            _strBuilder.AppendLine(Result.ToString());
+            _strBuilder.AppendLine("</td></tr>");
+        }
+
+        public void AddErrorDescriptionRows(List<string> Errors)
+        {
+            checkIsTableStarted();
+            checkIsLastRowError();
+
+            _strBuilder.AppendLine("<tr>");
+            _strBuilder.AppendLine("<td colspan=\"3\">");
+            _strBuilder.AppendLine("<table style=\"font-size: 80%; background: #bcb495; width: 80%\">");
+            _strBuilder.AppendLine("<tr><td style=\"color:#e50b0b\"><ul>");
+
+            Errors.ForEach((err) =>
+                {
+                    _strBuilder.AppendFormat("<li>{0}</li>", err);
+                });
+
+            _strBuilder.AppendLine("</ul></td></tr></table></td></tr>");
+        }
+
+        public void EndTable()
+        {
+            _strBuilder.AppendLine("</table>");
+            this._isTableStarted = false;
+        }
+
+        public override string ToString()
+        {
+            if (this._isTableStarted)
+                throw new Exception("Cannot format to string without closing the open table by calling EndTable method.");
+            return _strBuilder.ToString();
+        }
+
+        #region VerificationMethods
+        private void checkIsTableStarted()
+        {
+            if (!this._isTableStarted)
+                throw new Exception("Table has not yet been created. Run BeginTable method.");
+        }
+
+        private void checkIsLastRowError()
+        {
+            if (!this._isLastRowError)
+                throw new Exception("Cannot add error description. The previous row was not in error or critical status.");
+        }
+        #endregion VerificationMethods
+    }
+}
---
+++ FrontierVOps.FiOS.HealthCheckConsole/FrontierVOps.FiOS.HealthCheck.DataObjects/HealthRollup.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrontierVOps.FiOS.HealthCheck.DataObjects
+{
+    public class HealthRollup
+    {
+        public string ServerName { get; set; }
+        public StatusResult Result { get; set; }
+        public List<string> Errors { get; set; }
+
+        public HealthRollup()
+        {
+            this.Errors = new List<string>();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var hru = obj as HealthRollup;
+
+            if (hru == null)
+                throw new ArgumentException("Incorrect object type");
+
+            if (this.Errors.Count == hru.Errors.Count)
+            {
+                for (int i = 0; i < this.Errors.Count; i++)
+                {
+                    if (!this.Errors.Contains(hru.Errors[i]) || !hru.Errors.Contains(this.Errors[i]))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return this.Result.Equals(hru.Result) && this.ServerName.Equals(hru.ServerName);
+        }
+    }
+
+    public class HealthRollupCollection : ICollection<HealthRollup>
+    {
+        private List<HealthRollup> _innerHealthRollColl;
+
+        public HealthRollupCollection()
+        {
+            this._innerHealthRollColl = new List<HealthRollup>();
+        }
+
+        public IEnumerator<HealthRollup> GetEnumerator()
+        {
+            return this._innerHealthRollColl.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this._innerHealthRollColl.GetEnumerator();
+        }
+
+        public HealthRollup this[int index]
+        {
+            get { return (HealthRollup)_innerHealthRollColl[index]; }
+            set { _innerHealthRollColl[index] = value; }
+        }
+
+        public bool Contains(HealthRollup item)
+        {
+            bool found = false;
+
+            _innerHealthRollColl.ForEach((innerHru) =>
+                {
+                    if (innerHru.Equals(item))
+                        found = true;
+                });
+
+            return found;
+        }
+
+        public void Add(HealthRollup item)
+        {
+            if (!Contains(item))
+                _innerHealthRollColl.Add(item);
+        }
+
+        public void Clear()
+        {
+            _innerHealthRollColl.Clear();
+        }
+
+        public int Count
+        {
+            get { return _innerHealthRollColl.Count; }
+        }
+
+        public bool Remove(HealthRollup item)
+        {
+            for (int i = 0; i < _innerHealthRollColl.Count; i++)
+            {
+                if (_innerHealthRollColl[i].Equals(item))
+                {
+                    _innerHealthRollColl.RemoveAt(i);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool IsReadOnly()
+        {
+            return false;
+        }
+
+        public void CopyTo(HealthRollup[] array, int arrayIndex)
+        {
+            if (array == null)
+                throw new ArgumentNullException("The array cannot be null");
+            if (arrayIndex < 0)
+                throw new ArgumentOutOfRangeException("The starting index must not be negative");
+            if (Count > array.Length - arrayIndex + 1)
+                throw new ArgumentException("The destination array has fewer elements than the collection");
+
+            for (int i = 0; i < _innerHealthRollColl.Count; i++)
+                array[i + arrayIndex] = _innerHealthRollColl[i];
+        }
+    }
+}
