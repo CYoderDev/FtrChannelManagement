0e701061144ad7821d80ee194ac85a9520c9b9cd
cyy132 <cameron.yoder@ftr.com>
2017-09-22 10:31:15 -0400

    Added region controller to get active regions and vhos based on the application configuration file

---
+++ ChannelAPI/ChannelAPI/Controllers/RegionController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using ChannelAPI.Models;
+using ChannelAPI.Repositories;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ChannelAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class RegionController : Controller
+    {
+        #region PrivateFields
+        private IConfiguration _config;
+        private ILogger<StationController> _logger;
+        private RegionRepository _regionRepo;
+        #endregion PrivateFields
+
+        public RegionController(IConfiguration config, ILogger<StationController> logger, ILoggerFactory loggerFactory)
+        {
+            this._config = config;
+            this._logger = logger;
+            this._regionRepo = new RegionRepository(config, loggerFactory);
+        }
+
+        /// <summary>
+        /// Get all Fios Regions
+        /// </summary>
+        /// <returns>FiosRegion Json Format</returns>
+        /// <example>api/region</example>
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                _logger.LogTrace("Begin. params: none");
+                var response = await _regionRepo.GetAllAsync();
+
+                if (response == null || !response.Any())
+                {
+                    return NoContent();
+                }
+
+                return Json(response);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        /// <summary>
+        /// Get all active vho's based on application configuration
+        /// </summary>
+        /// <returns>string values in json format</returns>
+        /// <example>/api/region/vho</example>
+        [AllowAnonymous]
+        [HttpGet("vho")]
+        public async Task<IActionResult> GetActiveVhos()
+        {
+            try
+            {
+                _logger.LogTrace("Begin. params: none");
+                var response = await _regionRepo.GetActiveVHOs();
+                if (response == null || !response.Any())
+                    return NoContent();
+                return Json(response);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [AllowAnonymous]
+        [HttpGet("region/active")]
+        public async Task<IActionResult> GetActiveRegions()
+        {
+            try
+            {
+                _logger.LogTrace("Begin. params: none");
+                var response = await _regionRepo.GetActiveRegions();
+                if (response == null || !response.Any())
+                    return NoContent();
+                return Json(response);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return NotFound();
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ChannelAPI/ChannelAPI/Repositories/RegionRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using Dapper;
+using Dapper.Contrib.Extensions;
+using Dapper.Mapper;
+using ChannelAPI.Models;
+
+namespace ChannelAPI.Repositories
+{
+    public class RegionRepository
+    {
+        private string _version;
+        private ILogger _logger;
+        private IEnumerable<string> _vhos;
+
+        public RegionRepository(IConfiguration config, ILoggerFactory loggerFactory)
+        {
+            this._version = config.GetValue<string>("FiosChannelData:VersionAliasId");
+            this._logger = loggerFactory.CreateLogger<StationRepository>();
+            var vhoSection = config.GetSection("FiosChannelData:ActiveVHOs");
+            this._vhos = vhoSection.GetChildren().AsList().Select(x => x.Value);
+        }
+
+        public async Task<IEnumerable<FiosRegion>> GetAllAsync()
+        {
+            using (var connection = await DapperFactory.GetOpenConnectionAsync())
+            {
+                return await connection.GetAllAsync<FiosRegion>();
+            }
+        }
+
+        public async Task<IEnumerable<string>> GetActiveVHOs()
+        {
+            var query = new StringBuilder();
+            query.AppendLine("SELECT strVHOId FROM tFiosRegion WHERE strVHOId IN (@vhos)");
+            using (var connection = await DapperFactory.GetOpenConnectionAsync())
+            {
+                return await connection.QueryAsync<string>(query.ToString(), new { vhos = string.Join(',', this._vhos) });
+            }
+        }
+
+        public async Task<IEnumerable<string>> GetActiveRegions()
+        {
+            var query = "SELECT strFIOSRegionName FROM tFiosRegion WHERE strVHOId IN (@vhos)";
+
+            using (var connection = await DapperFactory.GetOpenConnectionAsync())
+            {
+                return await connection.QueryAsync<string>(query, new { vhos = string.Join(',', this._vhos) });
+            }
+        }
+    }
+}
